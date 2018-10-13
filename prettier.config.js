b51e1fa01d7f2e2d97d4096e4d566a74970087ca
adeelibr <adeelimranr@gmail.com>
2018-10-13 12:37:35 +0500

    prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,6 @@ module.exports = {
   trailingComma: 'all',
   bracketSpacing: true,
   jsxBracketSameLine: false,
-  parser: 'babylon', // If using type annotations use flow otherwise babylon works great
   tabWidth: 2,
   semi: true,
 };
