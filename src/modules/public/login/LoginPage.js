00ec8cad3ba1d4d093bd5b3ae85a7ebe977ad456
adeelibr <adeelimranr@gmail.com>
2018-10-16 00:00:23 +0500

    removed unused variable

--- src/modules/public/login/LoginPage.js
+++ src/modules/public/login/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
 // import axios from 'axios';
 // import styled from 'styled-components';
