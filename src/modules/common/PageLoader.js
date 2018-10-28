6de0110fd873520e66835204b28dbe0e36e8b6e6
adeelibr <adeelimranr@gmail.com>
2018-10-28 18:00:09 +0500

    added fancy circularprogress as pageloader

--- src/modules/common/PageLoader.js
+++ src/modules/common/PageLoader.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// Material UI
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const PageLoader = props => {
-  const { error, pastDelay, retry } = props;
-  if (error) {
-    return (
-      <div>
-        Error!{' '}
-        <button type="button" onClick={retry}>
-          Retry
-        </button>
-      </div>
-    );
-  }
-  if (pastDelay) {
-    return <div>Loading...</div>;
-  }
-  return null;
+const styles = theme => ({
+  progress: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    margin: theme.spacing.unit * 5,
+  },
+});
+
+const PageLoader = ({ classes }) => {
+  return (
+    <div className={classes.progress}>
+      <CircularProgress />
+    </div>
+  );
 };
 
 PageLoader.propTypes = {
-  error: PropTypes.any,
-  pastDelay: PropTypes.any,
-  retry: PropTypes.any,
+  classes: PropTypes.object.isRequired, // Material UI Injected
 };
 
-export default PageLoader;
+export default withStyles(styles)(PageLoader);
